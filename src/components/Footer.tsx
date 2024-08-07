07baf60a282171fd7a2606180bbdbd0b22964ac1
Narayan <amritanshmsh@gmail.com>
2024-08-07 09:36:05 +0530

    Fix:Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+;import React from 'react';
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-black py-4 text-sm text-white w-full">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center w-full px-4">
         <p className="mb-2 md:mb-0 text-center md:text-left">
-          Copyright © 2024 Waffle hackclub
+          Copyright © 2024 Waffle Hackclub
         </p>
-       
+        <div className="text-center flex flex-col md:flex-row gap-4">
+          <a href="/PrivacyPolicy" className="hover:underline">
+            Amity University Lucknow Campus
+          </a>
+         
+        </div>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
+
